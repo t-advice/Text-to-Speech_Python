e6857800b5b5e6be72cd8a8565bd6e9d27bd2637
Tashwill <tashwilladvice02@gmail.com>
2026-05-31 19:42:05 +0200

    warning resolved . I am installed the library "pip install pyttsx3" in the project terminal.

--- Text to Speech_Python/Text_to_Speech_Python.py
+++ Text to Speech_Python/Text_to_Speech_Python.py
@@ -3,7 +3,7 @@ import pyttsx3
 engine = pyttsx3.init()
 
 text_to_speech = "Hello World, the automation system is ready."
-engine.say(text_to_speech)
+engine.say(text_to_speech) # Pass the text to be spoken to the say() method of the engine object. This method queues the text for speech synthesis.
 engine.runAndWait()
 
 
